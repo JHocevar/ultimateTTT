9b05b8565d4cb5e053a07805d7d381879815b554
kikster500 <47098180+kikster500@users.noreply.github.com>
2019-12-05 09:16:12 -0600

    Updates

---
+++ precache-manifest.6ac74cb9cdd138ded1b903a3986721c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45093302c1c5d30958a6df3c3999cd48",
+    "url": "./index.html"
+  },
+  {
+    "revision": "162bd19f7d259c47bd77",
+    "url": "./static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "86731e833a9bfb81d3a7",
+    "url": "./static/css/main.a9f06c2a.chunk.css"
+  },
+  {
+    "revision": "162bd19f7d259c47bd77",
+    "url": "./static/js/2.3a81513c.chunk.js"
+  },
+  {
+    "revision": "86731e833a9bfb81d3a7",
+    "url": "./static/js/main.62afac02.chunk.js"
+  },
+  {
+    "revision": "56a6b2399647f3d207bd",
+    "url": "./static/js/runtime-main.f1c4a8af.js"
+  }
+]);
